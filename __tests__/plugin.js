faf5841f57fd25b4c84824de5eea459db68c9ef1
Dara Hak <dara.hak@gmail.com>
2017-03-04 01:21:50 +0100

    Added test.

---
+++ __tests__/plugin.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+const processor = require('../lib/processors/nativeFunction');
+
+function format(file) {
+  const filePath = path.join(__dirname, '../fixtures', file);
+  const text = fs.readFileSync(filePath, { encoding: 'utf-8' });
+  return processor.preprocess(text);
+}
+
+test('Replaces all occurrences of native functions with empty functions', () => {
+  const transformed = format('nativeDecl.js')[0];
+  expect(transformed).not.toContain('native');
+  expect(transformed).toContain('{}');
+});
