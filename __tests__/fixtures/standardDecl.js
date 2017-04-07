6b4608c4e3b3ff927fd89c0e0e47719c784f5974
Dara Hak <dara.hak@gmail.com>
2017-04-07 22:51:41 +0200

    Added a test for standard source code (no native keywords).

---
+++ __tests__/fixtures/standardDecl.js
@@ -0,0 +1,5 @@
+function a() {}
+var b = function() {};
+let c = function() {};
+const d = function() {};
+const e = () => {};
--- __tests__/plugin.js
+++ __tests__/plugin.js
@@ -2,14 +2,30 @@ const path = require('path');
 const fs = require('fs');
 const processor = require('../lib/processors/nativeFunction');
 
+function getFixturePath(file) {
+  return path.join(__dirname, 'fixtures', file);
+}
+
 function format(file) {
-  const filePath = path.join(__dirname, 'fixtures', file);
-  const text = fs.readFileSync(filePath, { encoding: 'utf-8' });
+  const text = fs.readFileSync(getFixturePath(file), { encoding: 'utf-8' });
   return processor.preprocess(text);
 }
 
 test('Replaces all occurrences of native functions with empty functions', () => {
-  const transformed = format('nativeDecl.js')[0];
-  expect(transformed).not.toContain('native');
+  const [transformed] = format('nativeDecl.js');
+  expect(transformed).not.toContain('native function');
   expect(transformed).toContain('function() {}');
 });
+
+test('Leaves source code untouched if it has no native function declarations', () => {
+  const fixture = 'standardDecl.js';
+  const [transformed] = format(fixture);
+  expect(transformed).not.toContain('native function');
+
+  const original = fs.readFileSync(getFixturePath(fixture), {
+    encoding: 'utf-8'
+  });
+  expect(transformed).toEqual(original);
+});
+
+// TODO: Write a test for postprocess().
